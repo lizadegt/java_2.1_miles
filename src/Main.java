cedcc4018a0b6ece3ccda158ae4edbc9455135e0
Liza Degtyareva <liza.degt@yandex.ru>
2021-08-12 20:41:29 +0300

    file gitignore+Main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 45_653;
+        int cost_mile = 20;
+        int bonus_miles = ticket_price / cost_mile;
+        System.out.println(bonus_miles);
+    }
+}
