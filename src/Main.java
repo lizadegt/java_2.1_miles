7cda2b328ad3e89000bf6ce3784ac6c97fb4c389
liza_degt <85506686+lizadegt@users.noreply.github.com>
2021-08-13 08:46:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 45_653;
-        int cost_mile = 20;
-        int bonus_miles = ticket_price / cost_mile;
-        System.out.println(bonus_miles);
+        int ticketPrice = 45_653;
+        int costMile = 20;
+        int bonusMiles = ticketPrice / costMile;
+        System.out.println(bonusMiles);
     }
 }
